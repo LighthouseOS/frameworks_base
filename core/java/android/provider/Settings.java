bf6ecfdee6064d562a4e884ee0000448a4301b99
Jason Edson <jaysonedson@gmail.com>
2020-09-28 12:25:37 -0700

    EnhancedEstimates: Get estimates from Device Health Services
    Get battery estimates from com.google.android.apps.turbo (Turbo.apk)
    Code taken from android11 SystemUIGoogle.apk and cleaned up
    
    Change-Id: Ic1d1cd4ed2865ffcaed1cab126f699ee4ddf32ab
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15250,6 +15250,12 @@ public final class Settings {
         public static final String ONE_HANDED_KEYGUARD_SIDE = "one_handed_keyguard_side";
 
         /**
+         * Google Hybrid Battery Warnings
+         * @hide
+         */
+        public static final String HYBRID_SYSUI_BATTERY_WARNING_FLAGS = "hybrid_sysui_battery_warning_flags";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -15257,7 +15263,6 @@ public final class Settings {
          * otherwise they won't be restored.
          *
          * @hide
-         */
         public static final String[] LEGACY_RESTORE_SETTINGS = {
         };
 
